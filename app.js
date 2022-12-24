61da19bf9abf4b79811bf4ead6ee3f2711e3b455
Sean Dylan Patterson <74496368+SeanDylan1982@users.noreply.github.com>
2022-12-24 17:08:29 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const slides = document.querySelectorAll(".slide");
+const gallery = document.querySelectorAll(".gallery");
+
+gallery.forEach(function(gallery, index){
+  gallery.style.left = `${index * 100}%`;
+});
 
 slides.forEach(function(slide, index){
   slide.style.left = `${index * 100}%`;
 });
 
-let counter = 0;
+let slidesCounter = 0;
+let galleryCounter = 0;
 
-function carousel(){
-  counter++;
-  if (counter === 6){
-    counter = 0;
+function slidesCarousel(){
+  slidesCounter++;
+  if (slidesCounter === 6){
+    slidesCounter = 0;
   }
   slides.forEach(function(slide){
-    slide.style.transform = `translateX(-${counter * 100}%)`;
+    slide.style.transform = `translateX(-${slidesCounter * 100}%)`;
   });
 };
-setInterval(carousel, 5000);
+setInterval(slidesCarousel, 5000);
 
+function galleryCarousel(){
+  galleryCounter++;
+  if (galleryCounter === 6){
+    galleryCounter = 0;
+  }
+  gallery.forEach(function(gallery){
+    gallery.style.transform = `translateX(-${galleryCounter * 100}%)`;
+  });
+};
+setInterval(galleryCarousel, 5000);
